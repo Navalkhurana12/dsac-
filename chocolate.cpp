ebe8de3f19d2dd6276df3a445540d9e592a6e3a4
navalkhurrana12 <nvlkhurana@gmail.com>
2025-10-01 18:45:30 +0530

    aa

---
+++ chocolate.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int n, int m, vector<int> a, vector<int> b)
+{
+  int i = 0, j = 0;
+  int hr = 1, vr = 1;
+  int count = 0;
+  while (i < n - 1 && j < m - 1)
+  {
+    if (a[i] >= b[j])
+    {
+      count += a[i] * vr;
+      hr++;
+      i++;
+    }
+    else
+    {
+      count += b[j] * hr;
+      vr++;
+      j++;
+    }
+  }
+  while (i < n - 1)
+    count += a[i++] * vr;
+  while (j < m - 1)
+    count += b[j++] * hr;
+  return count;
+}
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n - 1);
+  vector<int> b(m - 1);
+  for (int i = 0; i < n - 1; i++)
+  {
+    cin >> a[i];
+  }
+  for (int i = 0; i < m - 1; i++)
+  {
+    cin >> b[i];
+  }
+  sort(a.rbegin(), a.rend());
+  sort(b.rbegin(), b.rend());
+  int count = solve(n, m, a, b);
+  cout << count;
+}
